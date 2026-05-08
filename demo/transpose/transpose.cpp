e1dc943bc425778e8e55a05cc20d984d6677552a
牛奕博 <807239819@qq.com>
2026-05-08 23:57:29 +0800

    update demo

--- demo/transpose/transpose.cpp
+++ demo/transpose/transpose.cpp
@@ -28,6 +28,25 @@ bool isIdentityPermutation(llvm::ArrayRef<int32_t> permutation) {
   return true;
 }
 
+mlir::FailureOr<llvm::SmallVector<int32_t, 4>>
+invertPermutation(llvm::ArrayRef<int32_t> permutation) {
+  llvm::SmallVector<int32_t, 4> inverse(permutation.size(), -1);
+  for (int32_t index = 0; index < static_cast<int32_t>(permutation.size());
+       ++index) {
+    int32_t axis = permutation[index];
+    if (axis < 0 || axis >= static_cast<int32_t>(permutation.size()) ||
+        inverse[axis] != -1)
+      return mlir::failure();
+    inverse[axis] = index;
+  }
+
+  for (int32_t axis : inverse)
+    if (axis == -1)
+      return mlir::failure();
+
+  return inverse;
+}
+
 mlir::FailureOr<mlir::RankedTensorType>
 getPermutedTensorType(mlir::Type type, llvm::ArrayRef<int32_t> permutation) {
   auto ranked = llvm::dyn_cast<mlir::RankedTensorType>(type);
@@ -116,8 +135,38 @@ struct FoldTwoTransposesPattern final
   }
 };
 
-// transpose(unary(x), p) => unary(transpose(x, p)).
+// unary(transpose(x, p)) => transpose(unary(x), p).
 struct CombineTransposeUnaryPattern final
+    : public mlir::egraph::EGraphPatternFor<mlir::tosa::ExpOp> {
+  mlir::LogicalResult
+  matchAndRewrite(mlir::egraph::EOpRef<mlir::tosa::ExpOp> root,
+                  mlir::egraph::EGraphPatternRewriter &rewriter) const final {
+    return root.getOperand(0).matchDef<mlir::tosa::TransposeOp>(
+        [&](mlir::egraph::EOpRef<mlir::tosa::TransposeOp> transposeRef) {
+          mlir::tosa::TransposeOp transpose = transposeRef.getOp();
+          mlir::Value input = transpose.getInput1();
+          auto inputType =
+              llvm::dyn_cast<mlir::RankedTensorType>(input.getType());
+          if (!inputType)
+            return mlir::failure();
+
+          mlir::FailureOr<mlir::RankedTensorType> transposedType =
+              getPermutedTensorType(inputType, transpose.getPerms());
+          if (mlir::failed(transposedType))
+            return mlir::failure();
+
+          auto replacement = mlir::tosa::ExpOp::create(rewriter, root.getLoc(),
+                                                       inputType, input);
+          auto outerTranspose = mlir::tosa::TransposeOp::create(
+              rewriter, root.getLoc(), *transposedType,
+              replacement->getResult(0), transpose.getPerms());
+          return rewriter.replaceOp(root, outerTranspose->getResults());
+        });
+  }
+};
+
+// transpose(unary(x), p) => unary(transpose(x, p)).
+struct CombineUnaryTransposePattern final
     : public mlir::egraph::EGraphPatternFor<mlir::tosa::TransposeOp> {
   mlir::LogicalResult
   matchAndRewrite(mlir::egraph::EOpRef<mlir::tosa::TransposeOp> root,
@@ -137,6 +186,122 @@ struct CombineTransposeUnaryPattern final
           auto replacement = mlir::tosa::ExpOp::create(
               rewriter, root.getLoc(), *transposedType,
               innerTranspose->getResult(0));
+          return rewriter.replaceOp(root, replacement->getResults());
+        });
+  }
+};
+
+// binary(transpose(x, p), transpose(y, p)) => transpose(binary(x, y), p).
+struct CombineBinaryTransposePattern final
+    : public mlir::egraph::EGraphPatternFor<mlir::tosa::MaximumOp> {
+  mlir::LogicalResult
+  matchAndRewrite(mlir::egraph::EOpRef<mlir::tosa::MaximumOp> root,
+                  mlir::egraph::EGraphPatternRewriter &rewriter) const final {
+    mlir::tosa::MaximumOp binary = root.getOp();
+    return root.getOperand(0).matchDef<mlir::tosa::TransposeOp>(
+        [&](mlir::egraph::EOpRef<mlir::tosa::TransposeOp> leftRef) {
+          return root.getOperand(1).matchDef<mlir::tosa::TransposeOp>(
+              [&](mlir::egraph::EOpRef<mlir::tosa::TransposeOp> rightRef) {
+                mlir::tosa::TransposeOp leftTranspose = leftRef.getOp();
+                mlir::tosa::TransposeOp rightTranspose = rightRef.getOp();
+                if (leftTranspose.getPerms() != rightTranspose.getPerms())
+                  return mlir::failure();
+
+                mlir::Value leftInput = leftTranspose.getInput1();
+                mlir::Value rightInput = rightTranspose.getInput1();
+                auto leftType =
+                    llvm::dyn_cast<mlir::RankedTensorType>(leftInput.getType());
+                auto rightType = llvm::dyn_cast<mlir::RankedTensorType>(
+                    rightInput.getType());
+                if (!leftType || !rightType || leftType != rightType)
+                  return mlir::failure();
+
+                auto innerBinary = mlir::tosa::MaximumOp::create(
+                    rewriter, root.getLoc(), leftType, leftInput, rightInput);
+                auto replacement = mlir::tosa::TransposeOp::create(
+                    rewriter, root.getLoc(), binary.getOutput().getType(),
+                    innerBinary->getResult(0), leftTranspose.getPerms());
+                return rewriter.replaceOp(root, replacement->getResults());
+              });
+        });
+  }
+};
+
+// binary(transpose(x, p), y) => transpose(binary(x, transpose(y, invP)), p).
+struct CombineBinaryLeftTransposePattern final
+    : public mlir::egraph::EGraphPatternFor<mlir::tosa::MaximumOp> {
+  mlir::LogicalResult
+  matchAndRewrite(mlir::egraph::EOpRef<mlir::tosa::MaximumOp> root,
+                  mlir::egraph::EGraphPatternRewriter &rewriter) const final {
+    mlir::tosa::MaximumOp binary = root.getOp();
+    return root.getOperand(0).matchDef<mlir::tosa::TransposeOp>(
+        [&](mlir::egraph::EOpRef<mlir::tosa::TransposeOp> leftRef) {
+          mlir::tosa::TransposeOp leftTranspose = leftRef.getOp();
+          mlir::Value leftInput = leftTranspose.getInput1();
+          auto leftType =
+              llvm::dyn_cast<mlir::RankedTensorType>(leftInput.getType());
+          if (!leftType)
+            return mlir::failure();
+
+          mlir::FailureOr<llvm::SmallVector<int32_t, 4>> invPerm =
+              invertPermutation(leftTranspose.getPerms());
+          if (mlir::failed(invPerm))
+            return mlir::failure();
+
+          mlir::Value rightInput = root.getOperation()->getOperand(1);
+          auto rightInnerType =
+              getPermutedTensorType(rightInput.getType(), *invPerm);
+          if (mlir::failed(rightInnerType) || *rightInnerType != leftType)
+            return mlir::failure();
+
+          auto rightTranspose = mlir::tosa::TransposeOp::create(
+              rewriter, root.getLoc(), *rightInnerType, rightInput, *invPerm);
+          auto innerBinary = mlir::tosa::MaximumOp::create(
+              rewriter, root.getLoc(), leftType, leftInput,
+              rightTranspose->getResult(0));
+          auto replacement = mlir::tosa::TransposeOp::create(
+              rewriter, root.getLoc(), binary.getOutput().getType(),
+              innerBinary->getResult(0), leftTranspose.getPerms());
+          return rewriter.replaceOp(root, replacement->getResults());
+        });
+  }
+};
+
+// binary(x, transpose(y, p)) => transpose(binary(transpose(x, invP), y), p).
+struct CombineBinaryRightTransposePattern final
+    : public mlir::egraph::EGraphPatternFor<mlir::tosa::MaximumOp> {
+  mlir::LogicalResult
+  matchAndRewrite(mlir::egraph::EOpRef<mlir::tosa::MaximumOp> root,
+                  mlir::egraph::EGraphPatternRewriter &rewriter) const final {
+    mlir::tosa::MaximumOp binary = root.getOp();
+    return root.getOperand(1).matchDef<mlir::tosa::TransposeOp>(
+        [&](mlir::egraph::EOpRef<mlir::tosa::TransposeOp> rightRef) {
+          mlir::tosa::TransposeOp rightTranspose = rightRef.getOp();
+          mlir::Value rightInput = rightTranspose.getInput1();
+          auto rightType =
+              llvm::dyn_cast<mlir::RankedTensorType>(rightInput.getType());
+          if (!rightType)
+            return mlir::failure();
+
+          mlir::FailureOr<llvm::SmallVector<int32_t, 4>> invPerm =
+              invertPermutation(rightTranspose.getPerms());
+          if (mlir::failed(invPerm))
+            return mlir::failure();
+
+          mlir::Value leftInput = root.getOperation()->getOperand(0);
+          auto leftInnerType =
+              getPermutedTensorType(leftInput.getType(), *invPerm);
+          if (mlir::failed(leftInnerType) || *leftInnerType != rightType)
+            return mlir::failure();
+
+          auto leftTranspose = mlir::tosa::TransposeOp::create(
+              rewriter, root.getLoc(), *leftInnerType, leftInput, *invPerm);
+          auto innerBinary = mlir::tosa::MaximumOp::create(
+              rewriter, root.getLoc(), rightType, leftTranspose->getResult(0),
+              rightInput);
+          auto replacement = mlir::tosa::TransposeOp::create(
+              rewriter, root.getLoc(), binary.getOutput().getType(),
+              innerBinary->getResult(0), rightTranspose.getPerms());
           return rewriter.replaceOp(root, replacement->getResults());
         });
   }
@@ -182,8 +347,12 @@ mlir::LogicalResult runDemo(mlir::ModuleOp module) {
   mlir::Block &originalBlock = originalFunc.getBody().front();
 
   mlir::egraph::EGraphPatternSet patterns;
+  patterns.add<CombineBinaryTransposePattern>();
+  patterns.add<CombineBinaryLeftTransposePattern>();
+  patterns.add<CombineBinaryRightTransposePattern>();
   patterns.add<CombineTransposeBinaryPattern>();
   patterns.add<CombineTransposeUnaryPattern>();
+  patterns.add<CombineUnaryTransposePattern>();
   patterns.add<FoldTwoTransposesPattern>();
   patterns.add<FoldNopTransposePattern>();
 
